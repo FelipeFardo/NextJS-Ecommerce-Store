3cdad505471090b17c8988d171842eebea697db9
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-12-11 21:56:02 -0300

    feat: Individual Product Screen

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -10,14 +10,15 @@ const HomePage = async () => {
   const products = await getProducts({
     isFeatured: true
   })
+
   const billboard = await getBillboard('208d13bc-53b1-4b4b-bead-13cc0acc9a5b');
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   )
